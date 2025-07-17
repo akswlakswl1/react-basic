be04cfb7e4f1ff6976dfadcf24be08dd79c50e35
manjii <akswlakswl1@gmail.com>
2025-07-17 22:00:29 +0900

    add inroduce react

---
+++ my-react-app/src/Fotter.jsx
@@ -0,0 +1,13 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>
+                &copy; {new Date().getFullYear()} Your website web
+            </p>
+        </footer>
+    );
+
+}
+
+export default Footer
